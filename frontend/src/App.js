f856dbd9851395df02199d42de740bfd57f41a77
GitFolder <nlmphat2k4@gmail.com>
2025-12-18 08:52:48 +0700

    Update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
         <div>
-          <h1>Project 1 – React Frontend</h1>
-          <p>Frontend đã deploy thành công trên Vercel!</p>
+          <h1>Project 1</h1>
+          <p>Đã deploy thành công trên Vercel!</p>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
